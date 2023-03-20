220dacc43fc4921abd0a21e4df85cf32597e54dd
CedricPedric <cedricnelson03@gmail.com>
2023-03-20 12:34:26 +0100

    lab1/2 gedaan

---
+++ HelloWorld/hello.php
@@ -0,0 +1,16 @@
+<?php
+    $string = "Hello Monkey!";
+    $learningPHP = "Learning PHP";
+    $learningPHP = $string;
+
+    $hello = "Hello";
+    $world = "World!";
+
+    $arrayHelloWorld = ["Hello","World!"];
+
+    echo"$string <br>";
+    echo "$learningPHP";
+    echo "<h1> $hello $world</h1>";
+    echo "<h1> $arrayHelloWorld[0] $arrayHelloWorld[1]</h1>";
+?>
+
---
+++ Rekenen/sommen.php
@@ -0,0 +1,18 @@
+<?php
+    $randomnum = rand();
+    $randomnum2 = rand();
+    $calculation =  $randomnum + $randomnum2;
+
+    echo "<h2>Lab 3a</h2>";
+    echo "Num1: $randomnum + Num2: $randomnum2 = $calculation" ;
+?>
+
+
+<?php
+    $randomnum = rand();
+    $randomnum2 = rand();
+    $calculation =  $randomnum + $randomnum2;
+
+    echo "<h2>Lab 3b</h2>";
+    echo "Num1: $randomnum + Num2: $randomnum2 = $calculation" ;
+?>
